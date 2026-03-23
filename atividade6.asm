8468de3d74f487ed22abdce6d257ca00b5410cd2
mzckawa <5689.melzion.cmr@gmail.com>
2026-03-23 17:38:32 -0300

    Add LED control and button state handling in atividade6.asm
    Implement LED control logic based on button state.

---
+++ atividade6.asm
@@ -0,0 +1,22 @@
+addi x11, x0, 1 # armazenando um imediato que, no formato binário, acende o primeiro led (led 2, associado ao input 1 do arduino virtual)
+addi x12, x0, 1 # registrador que vai representar o botão ligado
+addi x14, x0, 32 # registrador que vai verificar se chegou ao último led 
+add x13, x0, x0 # registrador que vai representar o atual estado do botão
+sb x11, 1029(x0) # escrevendo 000001 nos pinos 2-7, para ligar apenas o 2
+sb x12, 1027(x0) # escrevendo zero nos pinos 8-13 (usaremos apenas o 8, entretanto)
+
+loop:
+
+lb x13, 1026(x0) # lendo o atual estado do botão 
+beq x13, x12, incrementacao # condicional que vai identificar quando o botão for apertado 
+beq x13, x14, fim
+jal x0, loop
+
+incrementacao: 
+add x11, x11, x11 # somando o registrador que liga ou desliga os pinos com ele mesmo, visando a dobrá-lo e, assim, fazer um shift left 
+sb x11, 1029(x0)
+
+jal x0, loop 
+
+fim: 
+halt 
