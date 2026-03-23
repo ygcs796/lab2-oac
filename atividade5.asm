96e30b78bb27e16b5b7b418024d5585515bc5c3e
mzckawa <5689.melzion.cmr@gmail.com>
2026-03-23 17:49:22 -0300

    Refactor comments and clean up whitespace in atividade5.asm
    Updated comments for clarity and removed unnecessary whitespace.

--- atividade5.asm
+++ atividade5.asm
@@ -1,6 +1,6 @@
-addi x11, x0, 42
+addi x11, x0, 42 # armazenando o valor de * em ASCII em um registrador 
 loop:
-lb x10, 1025(x0)
+lb x10, 1025(x0) 
 sb x10, 1024(x0)
 beq x10, x11, end
 jal x0, loop
